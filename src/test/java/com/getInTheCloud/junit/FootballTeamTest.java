5637a41670617d0bba927f89f5b9f7fc0212c444
David Crespo <davidcrespoarroyo@gmail.com>
2016-11-18 10:49:15 +0000

    Refactored the testing code to parametrise the test.

--- src/test/java/com/getInTheCloud/junit/FootballTeamTest.java
+++ src/test/java/com/getInTheCloud/junit/FootballTeamTest.java
@@ -1,20 +1,23 @@
 package com.getInTheCloud.junit;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by davicres on 17/11/2016.
  */
+@RunWith(JUnitParamsRunner.class)
 public class FootballTeamTest {
 
     @Test
-    public void constructorTest() {
-        int gamesWon = 0;
+    @Parameters({"0, 0", "3, 3", "5, 5"})
+    public void constructorTest(int gamesWon, int expected) {
         FootballTeam footballTeam = FootballTeam.build(gamesWon);
-        gamesWon = footballTeam.getGamesWon();
-        assertEquals(0, gamesWon);
+        assertEquals(expected, footballTeam.getGamesWon());
     }
 
 }
